35ec442f32b8c19380de315da1fb89759ecaae2e
nidhi22617 <137905795+nidhi22617@users.noreply.github.com>
2023-10-25 15:55:17 +0000

    ConversionOperation dene

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,19 +12,44 @@ let valueInNumber = Number (score)
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = "Nidhi"
+// let isLoggedIn = "Nidhi"
 
-let booleanIsLoggedIn = Boolean  (isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean  (isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true, 0 => false
 // "" => false
 // "Nidhi" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+//***************************************** Operations ****************************** */
+
+
+// let value = 3
+// let negValue = -value
+// console.log(value) 
+// console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = "Hitesh"
+
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log("1"+ 2)

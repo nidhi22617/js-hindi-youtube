8b214916c7714162e7c7b0286f67446a09e3ef80
nidhi22617 <137905795+nidhi22617@users.noreply.github.com>
2023-10-25 16:57:32 +0000

    DataTypes complete

--- 01_basics/DataTypes-summary.js
+++ 01_basics/DataTypes-summary.js
@@ -45,3 +45,29 @@ console.log(typeof myFunction)
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "hiteshchoudharydotcom"
+
+let anothername = myYoutubeName
+anothername = "chaiaurcode"
+
+console.log(myYoutubeName)
+console.log(anothername)
+
+let userOne = {
+    email: "user@google.com" ,
+    upi: "user@ybl"
+
+}
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
